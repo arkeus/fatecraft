6388ebb83dfb07d9ad662ee365caad562d268a12
Lee Miller <iarkeus@gmail.com>
2014-10-15 20:42:28 -0700

    Fix infinite super jump.

--- src/main/java/io/arkeus/fatecraft/listeners/AcrobaticsListener.java
+++ src/main/java/io/arkeus/fatecraft/listeners/AcrobaticsListener.java
@@ -3,17 +3,20 @@ package io.arkeus.fatecraft.listeners;
 import io.arkeus.fate.listener.FateListener;
 import io.arkeus.fatecraft.Fatecraft;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.util.Vector;
 
 public class AcrobaticsListener extends FateListener<Fatecraft> {
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onJoin(final PlayerJoinEvent event) {
-		event.getPlayer().setAllowFlight(true);
+	public void onMove(final PlayerMoveEvent event) {
+		if (((Entity) event.getPlayer()).isOnGround()) {
+			event.getPlayer().setAllowFlight(true);
+		}
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST)
@@ -25,6 +28,7 @@ public class AcrobaticsListener extends FateListener<Fatecraft> {
 		player.setFlying(false);
 		final Vector jump = player.getLocation().getDirection().multiply(0.25).setY(0.8);
 		player.setVelocity(player.getVelocity().add(jump));
+		player.setAllowFlight(false);
 		event.setCancelled(true);
 	}
 }
